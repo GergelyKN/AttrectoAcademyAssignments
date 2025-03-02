c96eb1a9a62193fad8e2208c3f5f1693dd5d86f4
GergelyKN <nagy.gergely.koppany@gmail.com>
2025-03-02 15:16:59 +0100

    Add new migration

--- Homework/Homework/Migrations/20250302135704_AddAgeToUsers.Designer.cs
+++ Homework/Homework/Migrations/20250302135704_AddAgeToUsers.Designer.cs
@@ -2,6 +2,7 @@
 using Homework.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Homework.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250302135704_AddAgeToUsers")]
+    partial class AddAgeToUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.13");
