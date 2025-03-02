5284eb585996440dc299b5d279485f4d50f240b7
GergelyKN <nagy.gergely.koppany@gmail.com>
2025-03-02 17:27:58 +0100

    Create CourseRepository, add basis CRUD methods

---
+++ Homework/Homework/Repositories/CourseRepository.cs
@@ -0,0 +1,59 @@
+﻿using Homework.Data;
+
+namespace Homework.Repositories
+{
+	public class CourseRepository
+	{
+		private readonly ApplicationDbContext _context;
+
+
+        public CourseRepository()
+        {
+            _context = new ApplicationDbContext();
+		}
+
+		public List<Course> GetAll()
+		{
+			return _context.Courses.ToList();
+		}
+
+		public Course? GetById(int id)
+		{
+			return _context.Courses.FirstOrDefault(c => c.Id == id);
+		}
+
+		public void Create(Course course)
+		{
+			_context.Courses.Add(course);
+			_context.SaveChanges();
+		}
+
+		public Course? Update(int id, Course updatedCourseData)
+		{
+			var course = GetById(id);
+			if(course != null)
+			{
+				course.Name = updatedCourseData.Name;
+				course.Description = updatedCourseData.Description;
+
+				_context.SaveChanges();
+
+				return course;
+			}
+			return null;
+		}
+
+		public bool Delete(int id)
+		{
+			var course = GetById(id);
+			if(course != null)
+			{
+				_context.Courses.Remove(course);
+				_context.SaveChanges();
+				return true;
+			}
+			return false;
+		}
+
+    }
+}
