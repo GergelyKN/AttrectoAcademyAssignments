2c80cd156e0a64ca3ea9b82e0f6aba3fa386e475
GergelyKN <nagy.gergely.koppany@gmail.com>
2025-03-11 20:18:53 +0100

    Register user and course repos in DI container; Add loophandling

--- Homework/Homework/Program.cs
+++ Homework/Homework/Program.cs
@@ -1,3 +1,7 @@
+using Homework.Data;
+using Homework.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace Homework
 {
@@ -8,9 +12,21 @@ namespace Homework
 			var builder = WebApplication.CreateBuilder(args);
 
 
-			builder.Services.AddControllers();
+			builder.Services.AddControllers()
+				.AddNewtonsoftJson(options =>
+				{	
+					options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+				});
+
 			builder.Services.AddEndpointsApiExplorer();
 			builder.Services.AddSwaggerGen();
+
+
+			builder.Services.AddScoped<IUserRepository, UserRepository>();
+			builder.Services.AddScoped<ICourseRepository, CourseRepository>();
+
+			builder.Services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlite(builder.Configuration.GetConnectionString("ApplicationDbContext")));
 
 			var app = builder.Build();
 
@@ -21,12 +37,8 @@ namespace Homework
 			}
 
 			app.UseHttpsRedirection();
-
 			app.UseAuthorization();
-
-
 			app.MapControllers();
-
 			app.Run();
 		}
 	}
