9107e70ad1bdb665ffe7ad266bf8b08f9038fbf6
GergelyKN <nagy.gergely.koppany@gmail.com>
2025-03-11 20:16:09 +0100

    Create interface

---
+++ Homework/Homework/Repositories/ICourseRepository.cs
@@ -0,0 +1,13 @@
+﻿using Homework.Data;
+
+namespace Homework.Repositories
+{
+	public interface ICourseRepository
+	{
+		void Create(Course course);
+		bool Delete(int id);
+		List<Course> GetAll();
+		Course? GetById(int id);
+		Course? Update(int id, Course updatedCourseData);
+	}
+}
---
+++ Homework/Homework/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Homework.Data;
+
+namespace Homework.Repositories
+{
+	public interface IUserRepository
+	{
+		void Create(User data);
+		bool Delete(int id);
+		List<User> GetAdultUsers();
+		List<User> GetAll();
+		User? GetById(int id);
+		User? Update(int id, User data);
+	}
+}
