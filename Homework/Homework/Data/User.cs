5adfd2cace144c331d0302d32390733008244b70
GergelyKN <nagy.gergely.koppany@gmail.com>
2025-03-11 20:26:30 +0100

    Remove FirstName,Lastname props; Add Role, Name, Courses props

--- Homework/Homework/Data/User.cs
+++ Homework/Homework/Data/User.cs
@@ -8,18 +8,22 @@ namespace Homework.Data
 		public int Id { get; set; }
 
 
-		[Required]
-		[MaxLength(50)]
-		public string? FirstName { get; set; }
-
-
-		[Required]
-		[MaxLength(50)]
-		public string? LastName { get; set; }
-
-
         [Required]
+        [MaxLength(50)]
+        public string? Name { get; set; }
+
+
+		[Required]
+		[MaxLength(15)]
+		public string? Role { get; set; }
+
+
+		[Required]
         [Range(0, 125, ErrorMessage = "Age must be between 0 and 125!")]
         public int Age { get; set; }
+
+
+		public List<Course>? Courses { get; set; } = [];
+
     }
 }
