ff10f072543ec11c782641ec4616e2693d0de10a
GergelyKN <nagy.gergely.koppany@gmail.com>
2025-03-20 23:03:30 +0100

    Create interface for CourseService with async methods

---
+++ Homework/Homework/Services/CourseServices/ICourseService.cs
@@ -0,0 +1,14 @@
+﻿using Homework.Dtos;
+
+namespace Homework.Services.CourseServices
+{
+	public interface ICourseService
+	{
+		Task<List<CourseDto>> GetAllAsync();
+		Task<CourseDto?> GetByIdAsync(int id);
+		Task CreateAsync(CourseDto courseDto);
+		Task<CourseDto?> UpdateAsync(int id, CourseDto courseDto);
+		Task<bool> DeleteAsync(int id);
+
+	}
+}
