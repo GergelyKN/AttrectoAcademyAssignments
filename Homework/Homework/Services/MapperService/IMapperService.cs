16e62c3e66fd39b52fb56a506523d1552f8c7502
GergelyKN <nagy.gergely.koppany@gmail.com>
2025-03-20 23:04:35 +0100

    Create interface for Mapperservice

---
+++ Homework/Homework/Services/MapperService/IMapperService.cs
@@ -0,0 +1,13 @@
+﻿using Homework.Data;
+using Homework.Dtos;
+
+namespace Homework.Services.MapperService
+{
+	public interface IMapperService
+	{
+		UserDto MapToDtoUser(User user);
+		User MapToModelUser(UserDto userDto);
+		Course MapToModelCourse(CourseDto courseDto);
+		CourseDto MapToDtoCourse(Course course);
+	}
+}
