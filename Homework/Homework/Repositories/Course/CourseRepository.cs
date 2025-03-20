3f06422c72a506aee4b321ac1c45b4df27588a3b
GergelyKN <nagy.gergely.koppany@gmail.com>
2025-03-20 22:58:08 +0100

    Refactor file, use async methods

---
+++ Homework/Homework/Repositories/Course/CourseRepository.cs
@@ -0,0 +1,55 @@
+﻿using Homework.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Homework.Repositories
+{
+	public class CourseRepository : ICourseRepository
+	{
+
+		private readonly ApplicationDbContext _context;
+
+		public CourseRepository(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+
+
+		public async Task<List<Course>> GetAllAsync()
+		{
+			return await _context.Courses.Include(c => c.Author).ToListAsync();
+		}
+
+
+		public async Task<Course?> GetByIdAsync(int id)
+		{
+			return await _context.Courses.Include(c => c.Author).FirstOrDefaultAsync(c => c.Id == id);
+		}
+
+
+		public async Task CreateAsync(Course course)
+		{
+			await _context.Courses.AddAsync(course);
+			await _context.SaveChangesAsync();
+		}
+
+
+		public async Task<int> UpdateAsync()
+		{
+			return await _context.SaveChangesAsync();
+		}
+
+
+		public async Task<bool> DeleteAsync(int id)
+		{
+			var course = await GetByIdAsync(id);
+			if (course != null)
+			{
+				_context.Courses.Remove(course);
+				await _context.SaveChangesAsync();
+				return true;
+			}
+			return false;
+		}
+	}
+}
