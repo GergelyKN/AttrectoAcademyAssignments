b3a6e4277ea79a67f2a495f106920942720f3f4f
GergelyKN <nagy.gergely.koppany@gmail.com>
2025-03-20 22:54:50 +0100

    Modified Snapshot

--- Homework/Homework/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Homework/Homework/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -17,6 +17,21 @@ namespace Homework.Migrations
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.13");
 
+            modelBuilder.Entity("CourseUser", b =>
+                {
+                    b.Property<int>("CoursesId")
+                        .HasColumnType("INTEGER");
+
+                    b.Property<int>("UsersId")
+                        .HasColumnType("INTEGER");
+
+                    b.HasKey("CoursesId", "UsersId");
+
+                    b.HasIndex("UsersId");
+
+                    b.ToTable("CourseUser", (string)null);
+                });
+
             modelBuilder.Entity("Homework.Data.Course", b =>
                 {
                     b.Property<int>("Id")
@@ -52,9 +67,17 @@ namespace Homework.Migrations
                     b.Property<int>("Age")
                         .HasColumnType("INTEGER");
 
+                    b.Property<string>("Email")
+                        .IsRequired()
+                        .HasColumnType("TEXT");
+
                     b.Property<string>("Name")
                         .IsRequired()
                         .HasMaxLength(50)
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("Password")
+                        .IsRequired()
                         .HasColumnType("TEXT");
 
                     b.Property<string>("Role")
@@ -67,19 +90,29 @@ namespace Homework.Migrations
                     b.ToTable("Users");
                 });
 
+            modelBuilder.Entity("CourseUser", b =>
+                {
+                    b.HasOne("Homework.Data.Course", null)
+                        .WithMany()
+                        .HasForeignKey("CoursesId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("Homework.Data.User", null)
+                        .WithMany()
+                        .HasForeignKey("UsersId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+                });
+
             modelBuilder.Entity("Homework.Data.Course", b =>
                 {
                     b.HasOne("Homework.Data.User", "Author")
-                        .WithMany("Courses")
+                        .WithMany()
                         .HasForeignKey("UserId")
-                        .OnDelete(DeleteBehavior.Cascade);
+                        .OnDelete(DeleteBehavior.Restrict);
 
                     b.Navigation("Author");
-                });
-
-            modelBuilder.Entity("Homework.Data.User", b =>
-                {
-                    b.Navigation("Courses");
                 });
 #pragma warning restore 612, 618
         }
